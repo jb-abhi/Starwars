9c31689f43b55fb710d48fccdd68e88f790da476
jb-abhi <jbabhi2@gmail.com>
2022-09-16 20:29:27 +0530

    loading state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 
 function App() {
   const [movies,setMovies]= useState([])
-  const [isLoading,setisLoading] = useState(false)
+  const [isLoading,setisLoading] = useState(false);
+  const [error,setError] =  useState('')
 
   // const dummyMovies = () => {
   //   fetch('https://swapi.py4e.com/api/films').then(response=>{
@@ -24,18 +25,27 @@ function App() {
   // }
 
   async function dummyMovies() {
-    setisLoading(true);
-    const response = await fetch('https://swapi.py4e.com/api/films')
-    const res = await response.json();
-    const transformedMovies = res.results.map(movie=>{
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          releaseDate: movie.release_date,
-          openingText: movie.opening_crawl
-        } 
-      })
-      setMovies(transformedMovies);
+
+    try{
+      setisLoading(true);
+      const response = await fetch('https://swapi.py4e.com/api/films')
+      if(!response.ok){
+        throw new Error('Something went wrong!');
+      }
+      const res = await response.json();
+      const transformedMovies = res.results.map(movie=>{
+          return {
+            id: movie.episode_id,
+            title: movie.title,
+            releaseDate: movie.release_date,
+            openingText: movie.opening_crawl
+          } 
+        })
+        setMovies(transformedMovies);
+      }
+      catch(error){
+        setError(error.message);
+      }
       setisLoading(false);
   }
 
@@ -46,7 +56,8 @@ function App() {
       </section>
       <section>
       {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-      {!isLoading && movies.length === 0 && <p>Found no movies</p>}
+      {!isLoading && movies.length === 0 && !error && <p>Found no movies</p>}
+      {!isLoading && error && <p>{error}</p>}
       {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
